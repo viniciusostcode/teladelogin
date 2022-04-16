855f65894d79072dd317439351f0f149c4e97204
Vinícius <viniciusostcontato@gmail.com>
2022-04-16 05:21:26 -0400

    adicinado validação de nome vazio e nome invalido

--- Tela Formulario/script.js
+++ Tela Formulario/script.js
@@ -1,7 +1,29 @@
-function CampoNulo() {
-    
+function CampoVazio (){
     if(document.getElementById("nome").value == ""){
-        alert('por favor digite alguma coisa');
+    alert('Por favor, preencha o campo nome');
+    document.getElementById("nome").focus();
+    return false
     }
-    
+    if(document.getElementById("email").value == ""){
+        alert('Por favor, preencha o campo Email');
+        document.getElementById("email").focus();
+        return false
+        }
+        if(document.getElementById("senha").value == ""){
+            alert('Por favor, preencha o campo senha');
+            document.getElementById("senha").focus();
+            return false
+            }
+            if(document.getElementById("Confirmsenha").value == ""){
+                alert('Por favor, confirme sua senha');
+                document.getElementById("Confirmsenha").focus();
+                return false
+                }
+    }
+function Nomeinvalido(){
+    if(document.getElementById("nome").value == "/\b[A-Za-zÀ-ú][A-Za-zÀ-ú]+,?\s[A-Za-zÀ-ú][A-Za-zÀ-ú]{2,19}\b/gi"){
+        alert('Por favor, Digite um nome válido');
+        document.getElementById("nome").focus();
+        return false
+        }
 }
