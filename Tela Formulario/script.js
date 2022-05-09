f68a1b923fbf10126cad150b6adf68f1fbee49db
Vinicius <fnkcontato@gmail.com>
2022-05-09 12:59:08 -0400

    Mudança 4

--- Tela Formulario/script.js
+++ Tela Formulario/script.js
@@ -1,28 +1,107 @@
-function Validar (){
+function Validar(){
     let nome = document.getElementById('nome');
     let email = document.getElementById('email');
     let senha = document.getElementById('senha');
-    let Confirmsenha = document.getElementById('Confirmsenha');
-    let regexemail = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+    let Confirmsenha = document.getElementById('Confirmsenha'); 
 
     if(nome.value.length == "" || nome.value.length <= 10){
-    alert('Hm... algo deu errado, por favor preencha o campo nome ou tente digitar um nome válido.');
-    } else {
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            background: '#00000052',
+            color: 'white',
+            font: 'Smooch Sans',
+            didOpen: (toast) => {
+              toast.addEventListener('mouseenter', Swal.stopTimer)
+              toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+          })
+          Toast.fire({
+            icon: 'error',
+            title: 'Hm... algo deu errado, por favor preencha o campo nome ou tente digitar um nome válido.'
+          }); 
+        } else {
         if(email.value.length == "" || email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1 || (email.value.indexOf(".") - email.value.indexOf("@") == 1)){
-    alert('Hm... algo deu errado, por favor preencha o campo email ou tente digitar um email válido.');
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                background: '#00000052',
+                color: 'white',
+                font: 'Smooch Sans',
+                didOpen: (toast) => {
+                  toast.addEventListener('mouseenter', Swal.stopTimer)
+                  toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+              })
+              Toast.fire({
+                icon: 'error',
+                title: 'Hm... algo deu errado, por favor preencha o campo email ou tente digitar um email válido.'
+              }); 
     } else {
-        if(senha.value.length == ""){
-    alert('Por favor, digite sua senha!');
+        if(senha.value.length == "" || senha.value.length <= 10 ){
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                background: '#00000052',
+                color: 'white',
+                font: 'Smooch Sans',
+                didOpen: (toast) => {
+                  toast.addEventListener('mouseenter', Swal.stopTimer)
+                  toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+              })
+              Toast.fire({
+                icon: 'error',
+                title: 'Hm... algo deu errado, por favor preencha o campo senha ou tente digitar uma senha com pelo menos 11 caracteres.'
+              }); 
     } else {
-        if(Confirmsenha.value.length == "" ){
-    alert('Por favor, confirme sua senha!');
+        if(Confirmsenha.value.length == "" || Confirmsenha.value !== senha.value ){
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                background: '#00000052',
+                color: 'white',
+                font: 'Smooch Sans',
+                didOpen: (toast) => {
+                  toast.addEventListener('mouseenter', Swal.stopTimer)
+                  toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+              })
+              Toast.fire({
+                icon: 'error',
+                title: 'Hm... algo deu errado, as senhas não conferem.'
+              }); 
    } else {
-        alert('Cadastrado com sucesso!');
-        location.reload();
+    setTimeout(function() {
+        // Do something after 5 seconds
+        location.reload();//reload page
+  }, 50000)
+  Swal.fire({
+    position: 'top',
+    icon: 'success',
+    title: 'Seu cadastro foi concluído com sucesso.',
+    showConfirmButton: false,
+    timer: 1500,
+    background: '#00000052',
+    color: 'white',
+  
+  })
+        
     }
 } 
 }
 }
 }
-
 
--- teste.js
+++
@@ -1,7 +0,0 @@
-const pass1 = 'senhafraca';
-const pass2 = '$3nh@f0rt3';
-
-const regex = /\W|_/;
-
-console.log(regex.test(pass1));
-console.log(regex.test(pass2));
